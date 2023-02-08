34b9b840e830a10db0d3876fb2923f189e4584a6
leoentersthevoid <c.leov@icloud.com>
2023-02-08 22:04:16 +0200

    path towards v2

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# hpatches
-[HPatches](https://hpatches.github.io/) dataset utils. I found it very difficult to work  with their package, so I decided to write my own.
+# HPatches v2
+
+The current format of the [HPatches](https://hpatches.github.io/) dataset may prove to be inadequate for evaluating deep learning models that typically process images, rather than patches. In order to effectively evaluate such models, it would be beneficial to have a dataset that comprises images with ground-truth keypoints, allowing for the comparison of descriptors extracted from corresponding regions (i.e. patches) in each image. The presence of a sufficient number of matches between these patches would then serve as an indicator of the robustness of the algorithm being evaluated.
+
+As a contribution to the research community, I am pursuing the development of such a dataset. I will update this repository progressively.
+
+# Citation
+```bibtex
+@InProceedings{hpatches_2017_cvpr,
+author={Vassileios Balntas and Karel Lenc and Andrea Vedaldi and Krystian Mikolajczyk},
+title = {HPatches: A benchmark and evaluation of handcrafted and learned local descriptors},
+booktitle = {CVPR},
+year = {2017}}
+```
--- download.sh
+++ download.sh
@@ -2,23 +2,20 @@
 
 base_url="http://icvl.ee.ic.ac.uk/vbalnt/hpatches"
 hpatches_url="$base_url/hpatches-release.tar.gz"
+hpatches_sequences="$base_url/hpatches-sequences-release.tar.gz"
 
-if [ $# -eq 0 ]; then
-    echo "Usage: "
-    echo "sh download.sh hpatches || downloads the patches dataset"
-    exit 1
-fi
-
-if test -d ./data/hpatches-release
+if test -d ./data/hpatches-release && test -d ./data/hpatches-sequences-release
 then
-echo "The ./data/hpatches-release directory already exists."
+echo "The dataset already exists!"
 exit 0
 fi
 
-echo "\n>> Please wait, downloading the HPatches patches dataset ~4.2G\n"
 mkdir -p ./data
-wget -O ./data/hpatches-release.tar.gz $hpatches_url
-echo ">> Please wait, extracting the HPatches patches dataset ~4.2G"
+echo ">> Downloading..."
+wget -O ./data/hpatches-release.tar.gz $hpatches_url --no-check-certificate
+wget -O ./data/hpatches-sequences-release.tar.gz $hpatches_sequences --no-check-certificate
+echo ">> Extracting..."
 tar -xzf ./data/hpatches-release.tar.gz -C ./data
-rm ./data/hpatches-release.tar.gz
-
+tar -xzf ./data/hpatches-sequences-release.tar.gz -C ./data
+rm ./data/hpatches-release.tar.gz ./data/hpatches-sequences-release.tar.gz
+echo ">> Done!"
